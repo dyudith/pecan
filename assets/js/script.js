c88b984544700672f62e9bf3d2d25ae0635cc523
dyudith <dyudith515@gmail.com>
2024-08-19 18:04:25 -0300

    primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('.flag').forEach(function(flag) {
+    flag.addEventListener('click', function() {
+        var selectedLanguage = this.getAttribute('data-lang');
+        var elementsToTranslate = document.querySelectorAll('[data-en]');
+
+        elementsToTranslate.forEach(function(element) {
+            var translation = element.getAttribute('data-' + selectedLanguage);
+            if (translation) {
+                element.textContent = translation;
+            }
+        });
+    });
+});
